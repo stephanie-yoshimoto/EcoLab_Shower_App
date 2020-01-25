0d5e064f7bc189dafd6f8a669d9fb9dd4d42aaef
Stephanie Yoshimoto <54461599+stephanie-yoshimoto@users.noreply.github.com>
2020-01-24 23:01:43 -0500

    initial commit

---
+++ EcoLab_Shower_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  EcoLab_Shower_AppTests.swift
+//  EcoLab_Shower_AppTests
+//
+//  Created by Stephanie Yoshimoto on 1/24/20.
+//  Copyright © 2020 Stephanie Yoshimoto. All rights reserved.
+//
+
+import XCTest
+@testable import EcoLab_Shower_App
+
+class EcoLab_Shower_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
