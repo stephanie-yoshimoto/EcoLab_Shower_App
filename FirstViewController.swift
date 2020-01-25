ef9f2da346aa2219fcf143e3e1208d8ef10dcbac
ananthr11 <54245613+ananthr11@users.noreply.github.com>
2020-01-25 12:05:00 -0500

    Add files via upload

---
+++ FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TabBar2
+//
+//  Created by Rashmi Ananth on 1/25/20.
+//  Copyright © 2020 Rashmi Ananth. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SecondViewController.swift
@@ -0,0 +1,83 @@
+//
+//  SecondViewController.swift
+//  TabBar2
+//
+//  Created by Rashmi Ananth on 1/25/20.
+//  Copyright © 2020 Rashmi Ananth. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    
+    var timer = Timer()
+    var (hours, minutes, seconds, fractions) = (0, 0, 0, 0)
+
+
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    @IBOutlet weak var fractionsLabel: UILabel!
+    
+    @IBOutlet weak var startOutlet: UIButton!
+    
+    @IBAction func start(_ sender: UIButton) {
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(SecondViewController.keepTimer), userInfo: nil, repeats: true)
+        
+        startOutlet.isHidden = true
+    }
+    
+    
+    @IBAction func pause(_ sender: UIButton) {
+        timer.invalidate()
+        startOutlet.isHidden = false
+    }
+    
+    
+    @IBAction func stop(_ sender: UIButton) {
+        timer.invalidate()
+        (hours, minutes, seconds, fractions) = (0, 0, 0, 0)
+        timeLabel.text = "00:00:00"
+        fractionsLabel.text = ".00"
+        startOutlet.isHidden = false
+    }
+    
+    
+    
+    @objc func keepTimer() {
+        
+        fractions += 1
+        if (fractions > 99) {
+            seconds += 1
+            fractions = 0
+        }
+        if (seconds == 60) {
+            minutes += 1
+            seconds = 0
+        }
+        if (minutes == 60) {
+            hours += 1
+            minutes = 0
+        }
+        
+        
+        let secondsString = seconds > 9 ? "\(seconds)" : "0\(seconds)"
+        
+        let minutesString = minutes > 9 ? "\(minutes)" : "0\(minutes)"
+        
+        let hoursString = hours > 9 ? "\(hours)" : "0\(hours)"
+        
+        timeLabel.text = "\(hoursString):\(minutesString):\(secondsString)"
+        fractionsLabel.text = ".\(fractions)"
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
