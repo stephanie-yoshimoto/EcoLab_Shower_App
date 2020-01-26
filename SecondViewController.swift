ec7eb2d8a1c1d1d216bca9ce78e16673ec6360e1
ananthr11 <54245613+ananthr11@users.noreply.github.com>
2020-01-26 00:23:08 -0500

    Add files via upload

--- SecondViewController.swift
+++ SecondViewController.swift
@@ -5,83 +5,66 @@
 //  Created by Rashmi Ananth on 1/25/20.
 //  Copyright © 2020 Rashmi Ananth. All rights reserved.
 //
-import UIKit
 
-struct Times {
-    static var times = [Float]()
-}
+import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseFirestore
 
 class SecondViewController: UIViewController {
-    var timer = Timer()
-    var (hours, minutes, seconds) = (0, 0, 0)
     
+    var db: Firestore!
+    var timer = Timer()
+    var (hours, minutes, seconds, fractions) = (0, 0, 0, 0)
+
     @IBOutlet weak var output: UILabel!
     
     @IBAction func save(_ sender: Any) {
         output.text = timeLabel.text
         
         UserDefaults.standard.set(timeLabel.text, forKey: "myShowerTimes")
-    }
-    
+          
+      }
+  
     
     @IBOutlet weak var timeLabel: UILabel!
     
-    @IBOutlet weak var startOutlet: UIButton!
+    @IBOutlet weak var fractionsLabel: UILabel!
+   
+    @IBOutlet weak var accountButton: UIButton!
     
-    @IBAction func start(_ sender: UIButton) {
-        
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(SecondViewController.keepTimer), userInfo: nil, repeats: true)
-        
-        startOutlet.isHidden = true
+    
+    
+    @IBOutlet weak var email: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    
+    @IBOutlet weak var passwordConfirm: UITextField!
+    
+    @IBAction func signUpAction(_ sender: Any) {
+    if password.text != passwordConfirm.text {
+        let alertController = UIAlertController(title: "Password Incorrect", message: "Please re-type password", preferredStyle: UIAlertController.Style.alert)
+        let defaultAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alertController.addAction(defaultAction)
+        present(alertController, animated: true, completion: nil)
+    } else {
+    Auth.auth().createUser(withEmail: email.text!, password: password.text!){ (user, error) in
+     if error == nil {
+       self.performSegue(withIdentifier: "signupToHome", sender: self)
+                    }
+     else{
+       let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+       let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                        
+        alertController.addAction(defaultAction)
+        self.present(alertController, animated: true, completion: nil)
+           }
+                }
+          }
     }
-    
-    
-    @IBAction func pause(_ sender: UIButton) {
-        timer.invalidate()
-        startOutlet.isHidden = false
-    }
-    
-    
-    @IBAction func cancel(_ sender: UIButton) {
-        timer.invalidate()
-        /*(hours, minutes, seconds) = (0, 0, 0, 0)*/
-        timeLabel.text = "00:00:00"
-        startOutlet.isHidden = false
-    }
-    
-    
-    
-    @objc func keepTimer() {
-        
-        seconds += 1
-        if (seconds == 60) {
-            minutes += 1
-            seconds = 0
-        }
-        if (minutes == 60) {
-            hours += 1
-            minutes = 0
-        }
-        
-        
-        let secondsString = seconds > 9 ? "\(seconds)" : "0\(seconds)"
-        
-        let minutesString = minutes > 9 ? "\(minutes)" : "0\(minutes)"
-        
-        let hoursString = hours > 9 ? "\(hours)" : "0\(hours)"
-        
-        timeLabel.text = "\(hoursString):\(minutesString):\(secondsString)"
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        if let x = UserDefaults.standard.object(forKey: "myShowerTimes") as? String {
-            output.text = x
-        }
-    }
-    
-    
     
     
     
     
 }
+
