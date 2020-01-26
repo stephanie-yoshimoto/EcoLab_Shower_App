1ad0e394c80d2d666fa24307a25021691bd59fba
ananthr11 <54245613+ananthr11@users.noreply.github.com>
2020-01-26 09:05:11 -0500

    Add files via upload

--- BadgesViewController.swift
+++ BadgesViewController.swift
@@ -1,20 +1,37 @@
 //
-//  BadgesController.swift
+//  BadgesViewController.swift
 //  TabBar2
 //
-//  Created by Stephanie Yoshimoto on 1/26/20.
+//  Created by Rashmi Ananth on 1/26/20.
 //  Copyright © 2020 Rashmi Ananth. All rights reserved.
 //
 
-import Foundation
 import UIKit
 
-class BadgesController: UIViewController {
+class BadgesViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        let alert = UIAlertController(title: "Rewards", message: "Points necessary to unlock reward: 200 pts", preferredStyle: .alert)
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var starbucksButton: UIButton!
+    @IBAction func showPopUp(_ sender: Any) {
+        let alert = UIAlertController(title: "Reward of the Day", message: "Points necessary to unlock reward: 200 pts", preferredStyle: .alert)
         
         alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
         self.present(alert, animated: true)
     }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
